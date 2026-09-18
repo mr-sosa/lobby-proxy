2659e892acab703a9f7a99fb5c22869fa2789f92
Manuel Sosa <mr.sosa@uniandes.edu.co>
2026-09-18 18:37:42 -0500

    fix: explicit 0.0.0.0 host binding for Render

--- src/index.ts
+++ src/index.ts
@@ -74,6 +74,6 @@ app.use('/api/lobby/*', async (req: Request, res: Response) => {
 });
 
 const port = parseInt(process.env.PORT || '3000');
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`listening on port ${port}`);
 });
